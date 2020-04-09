972040a62bf92fb85b379c813d600288615785a1
Patrick Kubiak <pk9948@rit.edu>
2020-04-09 17:44:45 -0400

    implement OMDb api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HackStackAPI
 
-Pulls movie data from tmdb
+Pulls movie and game data from [OMDb](https://www.omdbapi.com/), [TMDb](https://www.themoviedb.org/), and [IGDB](https://www.igdb.com/discover)
 
 ### Setup
 
@@ -14,12 +14,32 @@ yarn install
 node index.js
 ```
 
-### Example
+### Examples
 
-Pull json data from tmdb movie id `38700`
+Get data from OMDb using IMDb id `tt1502397`
 
 ```
-http://127.0.0.1:3000/rentalItem/movie/38700
+http://127.0.0.1:3000/rentalItem/movie/tt1502397
+```
+
+```
+{
+    "title": "Bad Boys for Life",
+    "itemID": "tt1502397",
+    "genre": "Action, Comedy, Crime, Thriller",
+    "itemDesc": "The Bad Boys Mike Lowrey and Marcus Burnett are back together for one last ride in the highly anticipated Bad Boys for Life.",
+    "contentRating": "R",
+    "yearReleased": "2020",
+    "rentalStatus": "",
+    "isLate": "",
+    "fine": ""
+}
+```
+
+Get data from TMDb using TMDb id `38700`
+
+```
+http://127.0.0.1:3000/rentalItemTMDb/movie/38700
 ```
 
 JSON Response:
@@ -38,7 +58,7 @@ JSON Response:
 }
 ```
 
-Get a list of games from igdb. id is unimplemented, this just gets a list of all games from igdb.
+Get a list of games from IGDB. id is unimplemented, this just gets a list of all games from IGDB.
 
 ```
 http://127.0.0.1:3000/rentalItem/game/1
--- index.js
+++ index.js
@@ -2,23 +2,35 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-const TMDB_API_KEY = "cf7c502592526f1498d082fd122d7309"
-const OMDB_API_KEY = "87daca5d"
-const IGDB_API_KEY = "fde293b445a4f938af26cdd01f53820a"
+const API_KEYS = Object.freeze({
+  OMDB: "87daca5d",
+  TMDB: "cf7c502592526f1498d082fd122d7309",
+  IGDB: "fde293b445a4f938af26cdd01f53820a",
+})
 
-const tmdb = require("moviedb")(TMDB_API_KEY)
-const apicalypse = require("apicalypse")
+const OmdbApi = require("omdb-api-pt")
+const omdb = new OmdbApi({
+  apiKey: API_KEYS.OMDB,
+})
+
+const tmdb = require("moviedb")(API_KEYS.TMDB)
 const axios = require("axios")
 
 app.get("/", (req, res) => res.send("Welcome to HackStackAPI!"))
 
-// OMDb implementation
-app.get("/rentalItem/movie/:id", (req, res) => {
-  const movieID = req.params.id
+/**
+ * OMDb implementation
+ * OMDb has more information that we need than IMDb.
+ *
+ * @param imdb IMDb id
+ * @return JSON movie object {title, itemID, genre, itemDesc, contentRating, yearReleased, rentalStatus, isLate, fine}
+ */
+app.get("/rentalItem/movie/:imdb", (req, res) => {
+  const movieID = req.params.imdb
 
   let movie = {
     title: "",
-    itemID: req.params.id.toString(),
+    itemID: movieID.toString(),
     genre: "",
     itemDesc: "",
     contentRating: "",
@@ -27,15 +39,32 @@ app.get("/rentalItem/movie/:id", (req, res) => {
     isLate: "",
     fine: "",
   }
+
+  // show is an IMDb ID string
+  omdb
+    .byId({
+      imdb: movieID.toString(),
+    })
+    .then((resOMDb) => {
+      //console.log(resOMDb)
+      movie.title = resOMDb.Title
+      movie.genre = resOMDb.Genre
+      movie.itemDesc = resOMDb.Plot
+      movie.contentRating = resOMDb.Rated
+      movie.yearReleased = resOMDb.Year
+      return res.status(200).json(movie)
+    })
 })
 
-// TMDb implementation
+/**
+ * TMDb implementation
+ */
 app.get("/rentalItemTMDb/movie/:id", (req, res) => {
   const movieID = req.params.id
 
   let movie = {
     title: "",
-    itemID: req.params.id.toString(),
+    itemID: movieID.toString(),
     genre: "",
     itemDesc: "",
     contentRating: "",
@@ -45,14 +74,13 @@ app.get("/rentalItemTMDb/movie/:id", (req, res) => {
     fine: "",
   }
 
-  tmdb.movieInfo({ id: movieID }, (err, resTMDB) => {
-    //console.log(resTMDB)
-    //console.log(resTMDB.popularity)
+  tmdb.movieInfo({ id: movieID }, (err, resTMDb) => {
+    //console.log(resTMDb)
     if (!err) {
-      movie.title = resTMDB.title
-      movie.genre = resTMDB.genres.map(({ name }) => name)
-      movie.itemDesc = resTMDB.overview
-      movie.yearReleased = resTMDB.release_date
+      movie.title = resTMDb.title
+      movie.genre = resTMDb.genres.map(({ name }) => name)
+      movie.itemDesc = resTMDb.overview
+      movie.yearReleased = resTMDb.release_date
       return res.status(200).json(movie)
     }
   })
@@ -67,7 +95,7 @@ app.get("/rentalItem/game/:id", (req, res) => {
     method: "POST",
     headers: {
       Accept: "application/json",
-      "user-key": IGDB_API_KEY,
+      "user-key": API_KEYS.IGDB,
     },
     // age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites
     data: "fields name,genres,summary,age_ratings,release_dates;",
