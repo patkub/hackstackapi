f60b964ecbc7aa60584aea9a2d974ac941d03092
Patrick Kubiak <pk9948@rit.edu>
2020-04-13 19:19:43 -0400

    Setup bootstrap

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use("/", appRoutes)
 const apiRoutes = require("./routes/api")
 app.use("/api", apiRoutes)
 
+app.use(express.static(__dirname + "/node_modules/bootstrap/dist"))
+app.use(express.static(__dirname + "/node_modules/jquery/dist"))
+
 app.listen(config.port, () =>
   console.log(`HackStackAPI listening at http://localhost:${config.port}`)
 )
