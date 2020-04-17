2a3bf5bbc5700351e22162209bb0baace823882e
Patrick Kubiak <pk9948@rit.edu>
2020-04-17 13:56:29 -0400

    setup routes for adding movie, renting movie, and inventory report

--- routes/app/root.js
+++ routes/app/root.js
@@ -15,4 +15,37 @@ router
     )
   })
 
+  // adding a movie
+  .get("/addMovie", (req, res) => {
+    fs.readFile(
+      path.resolve(__dirname, "./views/addMovie.html"),
+      "utf8",
+      function (err, contents) {
+        res.send(contents)
+      }
+    )
+  })
+
+  // renting a movie
+  .get("/rentMovie", (req, res) => {
+    fs.readFile(
+      path.resolve(__dirname, "./views/rentMovie.html"),
+      "utf8",
+      function (err, contents) {
+        res.send(contents)
+      }
+    )
+  })
+
+  // inventory report
+  .get("/inventoryReport", (req, res) => {
+    fs.readFile(
+      path.resolve(__dirname, "./views/inventoryReport.html"),
+      "utf8",
+      function (err, contents) {
+        res.send(contents)
+      }
+    )
+  })
+
 module.exports = router
