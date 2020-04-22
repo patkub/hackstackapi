60b2401b83afd7c4203bc24d88b3df984f1d37a3
Patrick Kubiak <pk9948@rit.edu>
2020-04-22 17:40:54 -0400

    Fill out add movie form from OMDb API

--- routes/app/views/js/addMovie.js
+++ routes/app/views/js/addMovie.js
@@ -1,35 +1,83 @@
 $(function () {
-  // render the navbar
-  const navbar = new HackStackNavBar("addMovie")
-  navbar.inject("#navbar")
+  window.hackstack = window.hackstack || {}
+  ;(function (hackstack) {
+    // render the navbar
+    const navbar = new HackStackNavBar("addMovie")
+    navbar.inject("#navbar")
 
-  $("#addNewMovie").submit(function (e) {
-    const new_movie = {
-      title: $("#inputTitle").val(),
-      itemID: $("#inputId").val(),
-      genre: $("#inputGenre").val(),
-      itemDesc: $("#inputDesc").val(),
-      contentRating: $("#inputContentRating").val(),
-      yearReleased: $("#inputYear").val(),
-      rentalStatus: "",
-      isLate: "",
-      fine: [],
-    }
-    console.log(new_movie)
+    $("#imdbID").tooltip({
+      trigger: "focus",
+      title: "Link to movie on imdb.com",
+      placement: "bottom",
+    })
 
-    let data = {
-      title: $("#inputTitle").val(),
-      year: $("#inputYear").val(),
-      itemID: $("#inputId").val(),
-      genre: $("#inputGenre").val(),
-      description: $("#inputDesc").val(),
-      rating: $("inputContentRating").val(),
-    }
+    $("#btnFill").click(function (e) {
+      // extract imdb id, supported formats:
+      // 1) https://www.imdb.com/title/tt1502397/
+      // 2) tt1502397
+      const imdb_id = $("#imdbID").val().match(/tt\d+/)
 
-    let posted = $.post("http://localhost:8080/movies/add", data)
+      // http://www.omdbapi.com/?apikey=[yourkey]&
+      const omdb_url = "http://www.omdbapi.com/?apikey={0}&i={1}".format(
+        hackstack.API_KEYS.OMDB,
+        imdb_id
+      )
 
-    //it will return a boolean with whether or not the item was added
+      const new_movie = {
+        title: "",
+        itemID: "",
+        genre: "",
+        itemDesc: "",
+        contentRating: "",
+        yearReleased: "",
+        rentalStatus: "",
+        isLate: "",
+        fine: [],
+      }
 
-    return false
-  })
+      $.getJSON(omdb_url, function (data) {
+        new_movie.title = data.Title
+        new_movie.year = data.Year
+        new_movie.genre = data.Genre
+        new_movie.description = data.Plot
+        new_movie.contentRating = data.Rated
+
+        $("#inputTitle").val(new_movie.title)
+        $("#inputYear").val(new_movie.year)
+        $("#inputGenre").val(new_movie.genre)
+        $("#inputDesc").val(new_movie.description)
+        $("#inputContentRating").val(new_movie.contentRating)
+      })
+    })
+
+    $("#addNewMovie").submit(function (e) {
+      const new_movie = {
+        title: $("#inputTitle").val(),
+        itemID: $("#inputId").val(),
+        genre: $("#inputGenre").val(),
+        itemDesc: $("#inputDesc").val(),
+        contentRating: $("#inputContentRating").val(),
+        yearReleased: $("#inputYear").val(),
+        rentalStatus: "",
+        isLate: "",
+        fine: [],
+      }
+      console.log(new_movie)
+
+      let data = {
+        title: $("#inputTitle").val(),
+        year: $("#inputYear").val(),
+        itemID: $("#inputId").val(),
+        genre: $("#inputGenre").val(),
+        description: $("#inputDesc").val(),
+        rating: $("inputContentRating").val(),
+      }
+
+      let posted = $.post("http://localhost:8080/movies/add", data)
+
+      //it will return a boolean with whether or not the item was added
+
+      return false
+    })
+  })(window.hackstack)
 })
--- routes/app/views/js/utils.js
+++ routes/app/views/js/utils.js
@@ -6,6 +6,12 @@ window.hackstack.API_SERVER = "http://127.0.0.1:8080/"
 // the express.js api for testing purposes
 //window.hackstack.API_SERVER = "http://127.0.0.1:3000/api/"
 
+window.hackstack.API_KEYS = Object.freeze({
+  OMDB: "87daca5d",
+  TMDB: "cf7c502592526f1498d082fd122d7309",
+  IGDB: "fde293b445a4f938af26cdd01f53820a",
+})
+
 if (!String.prototype.format) {
   String.prototype.format = function () {
     var args = arguments
